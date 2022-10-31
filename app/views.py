e90a346853d50ed96a1ba9834121d26a8dd0a57c
hafidh0108 <hafidh.difa@gmail.com>
2022-10-31 12:50:08 +0700

    Update Views Portofolio Detail

--- app/views.py
+++ app/views.py
@@ -49,6 +49,7 @@ def portfolio_detail(request, pk):
         konteks = {
             'projek' : projek
         }
+        activity('detail portofolio')
         return render(request, 'portfolio_detail.html', konteks)
     except models.Projek.DoesNotExist:
         return HttpResponseNotFound('')
